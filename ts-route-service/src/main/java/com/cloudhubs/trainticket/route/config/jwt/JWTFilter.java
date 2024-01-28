745e7273acde7e127186ecd5df2e91fced1d923c
sakhawathossen58 <sakhawathossen58@gmail.com>
2024-01-28 11:43:30 -0700

    removed spring security till station service

--- ts-route-service/src/main/java/com/cloudhubs/trainticket/route/config/jwt/JWTFilter.java
+++ ts-route-service/src/main/java/com/cloudhubs/trainticket/route/config/jwt/JWTFilter.java
@@ -1,32 +1,32 @@
-package com.cloudhubs.trainticket.route.config.jwt;
-
-import io.jsonwebtoken.JwtException;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.web.filter.OncePerRequestFilter;
-
-import jakarta.servlet.FilterChain;
-import jakarta.servlet.ServletException;
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
-import java.io.IOException;
-
-/**
- * @author fdse
- */
-public class JWTFilter extends OncePerRequestFilter {
-
-    @Override
-    protected void doFilterInternal(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, FilterChain filterChain) throws ServletException, IOException {
-
-        try {
-            Authentication authentication =
-                    JWTUtil.
-                            getJWTAuthentication(httpServletRequest);
-            SecurityContextHolder.getContext().setAuthentication(authentication);
-            filterChain.doFilter(httpServletRequest, httpServletResponse);
-        } catch (JwtException e) {
-            httpServletResponse.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
-        }
-    }
-}
+//package com.cloudhubs.trainticket.route.config.jwt;
+//
+//import io.jsonwebtoken.JwtException;
+//import org.springframework.security.core.Authentication;
+//import org.springframework.security.core.context.SecurityContextHolder;
+//import org.springframework.web.filter.OncePerRequestFilter;
+//
+//import jakarta.servlet.FilterChain;
+//import jakarta.servlet.ServletException;
+//import jakarta.servlet.http.HttpServletRequest;
+//import jakarta.servlet.http.HttpServletResponse;
+//import java.io.IOException;
+//
+///**
+// * @author fdse
+// */
+//public class JWTFilter extends OncePerRequestFilter {
+//
+//    @Override
+//    protected void doFilterInternal(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, FilterChain filterChain) throws ServletException, IOException {
+//
+//        try {
+//            Authentication authentication =
+//                    JWTUtil.
+//                            getJWTAuthentication(httpServletRequest);
+//            SecurityContextHolder.getContext().setAuthentication(authentication);
+//            filterChain.doFilter(httpServletRequest, httpServletResponse);
+//        } catch (JwtException e) {
+//            httpServletResponse.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+//        }
+//    }
+//}
