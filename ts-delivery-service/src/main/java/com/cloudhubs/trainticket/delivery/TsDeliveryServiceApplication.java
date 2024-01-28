1af9f49c0b597fcb6418de00cfba12a5592d02e8
Ashfakur Rahman Arju <ashfak@Ashfakurs-MacBook-Pro.local>
2024-01-27 20:40:42 -0700

    Gateway config changed

--- ts-delivery-service/src/main/java/com/cloudhubs/trainticket/delivery/TsDeliveryServiceApplication.java
+++ ts-delivery-service/src/main/java/com/cloudhubs/trainticket/delivery/TsDeliveryServiceApplication.java
@@ -3,6 +3,7 @@ package com.cloudhubs.trainticket.delivery;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
